7a19aa8fdc2e7634a639a76877e4b28ad5b094ca
Young <shanghaiyangming@gmail.com>
2013-10-14 00:38:39 +0800

    初始化提交

---
+++ module/Application/src/Application/Controller/IndexController.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+        $events = $this->getEventManager();
+        echo $this->params()->fromRoute('index');
+        echo $this->params()->fromQuery('get');
+        echo $this->params()->fromFiles('file');
+        echo $this->params()->fromRoute('r');
+        $events->attach('do', function ($e)
+        {
+            $event = $e->getName();
+            $params = $e->getParams();
+            printf('Handled event "%s", with parameters %s', $event, json_encode($params));
+        });
+        $params = array(
+            'foo' => 'bar',
+            'baz' => 'bat'
+        );
+        $events->trigger('do', null, $params);
+        return new ViewModel();
+    }
+
+    public function noViewAction()
+    {
+        // 不适用view的情况
+        phpinfo();
+        return $this->response;
+    }
+}
