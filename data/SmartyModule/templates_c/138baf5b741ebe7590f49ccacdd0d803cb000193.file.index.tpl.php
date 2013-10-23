39a36f11503885652b8c50d2dcc40f0475ba69d7
Young <shanghaiyangming@gmail.com>
2013-10-23 23:07:38 +0800

    更换模板尝试

---
+++ data/SmartyModule/templates_c/138baf5b741ebe7590f49ccacdd0d803cb000193.file.index.tpl.php
@@ -0,0 +1,25 @@
+<?php /* Smarty version Smarty-3.1-DEV, created on 2013-10-23 14:19:59
+         compiled from "D:\MyCode\iCC\module\Application\view\application\index\index.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:226395267db0f37f3a4-18381782%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '138baf5b741ebe7590f49ccacdd0d803cb000193' => 
+    array (
+      0 => 'D:\\MyCode\\iCC\\module\\Application\\view\\application\\index\\index.tpl',
+      1 => 1382537994,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '226395267db0f37f3a4-18381782',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1-DEV',
+  'unifunc' => 'content_5267db0f3f74b1_63263266',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5267db0f3f74b1_63263266')) {function content_5267db0f3f74b1_63263266($_smarty_tpl) {?><html>
+<body>
+</body>
+</html><?php }} ?>
