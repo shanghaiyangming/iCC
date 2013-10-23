2b3346ad7e0241913e0e2c94bc6ef52205ca0df1
Young <shanghaiyangming@gmail.com>
2013-10-23 20:17:45 +0800

    增加注释

--- config/constant.php
+++ config/constant.php
@@ -1,7 +1,6 @@
 <?php
-
 /**
  * 定义全局的常量
  */
 defined('ROOT_PATH') || define('ROOT_PATH', dirname(__DIR__));
-defined('CACHE_ADAPTER') || define('CACHE_ADAPTER', 'fileCache');
+defined('CACHE_ADAPTER') || define('CACHE_ADAPTER', 'fileCache');//[fileCache|memcachedCache|redisCache]
