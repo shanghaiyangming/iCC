3a9dc29cab109bc72ba6a248ad70a81378debedd
yangming <shanghaiyangming@gmail.com>
2013-10-24 10:13:22 +0800

    使用默认模板

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -110,34 +110,17 @@ return array(
     'controllers' => array(
         'invokables' => array()
     ),
-//     'view_manager' => array(
-//         'display_not_found_reason' => true,
-//         'display_exceptions' => true,
-//         'doctype' => 'HTML5',
-//         'not_found_template' => 'error/404',
-//         'exception_template' => 'error/index',
-//         'template_map' => array(
-//             'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
-//             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-//             'error/404' => __DIR__ . '/../view/error/404.phtml',
-//             'error/index' => __DIR__ . '/../view/error/index.phtml'
-//         ),
-//         'template_path_stack' => array(
-//             __DIR__ . '/../view'
-//         )
-//     ),
     'view_manager' => array(
-        'default_suffix' => 'tpl', // <-- new option for path stack resolver
         'display_not_found_reason' => true,
         'display_exceptions' => true,
         'doctype' => 'HTML5',
         'not_found_template' => 'error/404',
         'exception_template' => 'error/index',
         'template_map' => array(
-            'layout/layout' => __DIR__ . '/../view/layout/layout.tpl',
-            'application/index/index' => __DIR__ . '/../view/application/index/index.tpl',
-            'error/404' => __DIR__ . '/../view/error/404.tpl',
-            'error/index' => __DIR__ . '/../view/error/index.tpl'
+            'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+            'error/404' => __DIR__ . '/../view/error/404.phtml',
+            'error/index' => __DIR__ . '/../view/error/index.phtml'
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view'
