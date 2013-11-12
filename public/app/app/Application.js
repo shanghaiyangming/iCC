07a16a1c0ba63ba1a2cc4513e0488a4b5106d7dc
yangming <shanghaiyangming@gmail.com>
2013-11-12 11:16:44 +0800

    创建ext项目工程目录

---
+++ public/app/app/Application.js
@@ -0,0 +1,17 @@
+Ext.define('icc.Application', {
+    name: 'icc',
+
+    extend: 'Ext.app.Application',
+
+    views: [
+        // TODO: add views here
+    ],
+
+    controllers: [
+        // TODO: add controllers here
+    ],
+
+    stores: [
+        // TODO: add stores here
+    ]
+});
