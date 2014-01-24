3e72f76965c9c0e10eb450061491a28e2f3b8d8c
yangming <shanghaiyangming@gmail.com>
2014-01-24 11:42:56 +0800

    更新

--- module/Idatabase/src/Idatabase/Model/Plugin.php
+++ module/Idatabase/src/Idatabase/Model/Plugin.php
@@ -6,32 +6,7 @@ use My\Common\Model\Mongo;
 class Plugin extends Mongo
 {
 
-    private $_project_plugin;
-
-    private $_plugin_collection;
-    
-    private $_plugin_structure;
-
-    private $_structure;
-
-    private $_collection;
-
-    private $_project;
-
-    private $_mapping;
-
     protected $collection = IDATABASE_PLUGINS;
-
-    public function init()
-    {
-        $this->_project_plugin = new ProjectPlugin($this->config);
-        $this->_plugin_collection = new PluginCollection($this->config);
-        $this->_plugin_structure = new PluginStructure($this->config);
-        $this->_structure = new Structure($this->config);
-        $this->_collection = new Collection($this->config);
-        $this->_project = new Project($this->config);
-        $this->_mapping = new Mapping($this->config);
-    }
 
     /**
      * 同步全部plugin_id的文档自定义结构
@@ -40,5 +15,4 @@ class Plugin extends Mongo
      */
     public function syncAll($plugin_id)
     {}
-
 }
